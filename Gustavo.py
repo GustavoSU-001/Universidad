ccb0c02acb7e29a8123341fc06d26d81e7bb2cdc
GustavoSU-001 <uribeadolfo24@gmail.com>
2025-10-11 21:36:30 -0300

    a

---
+++ Gustavo.py
@@ -0,0 +1,33 @@
+import pymysql
+def Conexion(self):
+    def __init__(self):
+        
+        self.db = pymysql.connect(
+            host='localhost',
+            user='root',
+            password='',
+            database='ev1_inteligencianegocios',
+            cursorclass=pymysql.cursors.DictCursor
+        )
+        self.cursor = self.db.cursor()
+    
+    #
+    #
+    #if tipo in ['delete','select']:
+    #    cursor.execute(consulta)
+    #else:
+    #    cursor.execute(consulta,datos)
+    #    
+    # 
+    #if tipo == 'insert':
+    #    db.commit()
+    #elif tipo == 'select':
+    #    resultado = cursor.fetchall()
+    #    cursor.close()
+    #    return resultado
+    #elif tipo == 'update':
+    #    db.commit()
+    #elif tipo == 'delete':
+    #    db.commit()
+    # 
+    #cursor.close()
